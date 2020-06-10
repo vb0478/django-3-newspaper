f4b4546d6d0caaef7961a1c7f8a05a404edd5452
vb0478 <vb047851@student.ath.edu.pl>
2020-06-10 11:58:48 +0200

    updated article views

--- articles/urls.py
+++ articles/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     path('<int:pk>/edit/', ArticleUpdateView.as_view(), name='article_edit'), # new
     path('<int:pk>/', ArticleDetailView.as_view(), name='article_detail'), # new
     path('<int:pk>/delete/', ArticleDeleteView.as_view(), name='article_delete'), # new
-    path('articles/', ArticleListView.as_view(), name='article_list'),
+    path('', ArticleListView.as_view(), name='article_list'),
 ]
